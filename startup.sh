26f5d70ffd3806ab1f271a308b81a941edf01318
AryaMewada <aryamewada25@gmail.com>
2026-05-21 14:37:22 +0530

    fix startup.sh absolute paths

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp config.azure.yaml config.yaml
+cp /home/site/wwwroot/config.azure.yaml /home/site/wwwroot/config.yaml
 pip install -r requirements.txt --quiet
 gunicorn --bind 0.0.0.0:8000 --timeout 120 --workers 1 worker:app
