fad93608638db411731f593fdb8a3244e95fd3ec
AryaMewada <aryamewada25@gmail.com>
2026-05-21 12:32:31 +0530

    add azure config and fix startup

--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# startup.sh
-# Run by Azure App Service on container start.
-# Installs dependencies and starts gunicorn pointing to worker:app
-
+cp config.azure.yaml config.yaml
 pip install -r requirements.txt --quiet
 gunicorn --bind 0.0.0.0:8000 --timeout 120 --workers 1 worker:app
